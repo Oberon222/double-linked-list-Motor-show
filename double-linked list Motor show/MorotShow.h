8cdb9cb8ce5da3dda4d3c904669d5afd8f2c203d
Pavlo Kostenko <pavelkost222@gmail.com>
2020-07-20 11:50:40 +0300

    initial commit

--- double-linked list Motor show/MorotShow.h
+++ double-linked list Motor show/MorotShow.h
@@ -3,11 +3,12 @@
 #include<string>
 #include<list>
 #include"Car.h"
+#include<vector>
 using namespace std;
 
 class MotorShow
 {
-	list< Car> cars;
+	vector< Car> cars;
 
 public:
 
@@ -36,47 +37,28 @@ public:
 
 	void SortByNane()
 	{
-		cars.sort(CompareCarByName);
-		for (Car c : cars)
-		{
-			c.ShowCarInfo();
-		}
+		
 	}
 
 	void SortByGraduationYear()
 	{
-		cars.sort(CompareCarByGraduationYear);
-		for (Car c : cars)
-		{
-			c.ShowCarInfo();
-		}
+		
 	}
 
 	void SortByEngineCapacity()
 	{
-		cars.sort(CompareCarByEngineCapacity);
-		for (Car c : cars)
-		{
-			c.ShowCarInfo();
-		}
+		
 	}
 
 	void SortByPrice()
 	{
-		cars.sort(CompareCarByPrice);
-		for (Car c : cars)
-		{
-			c.ShowCarInfo();
-		}
+		
 	}
 
 	void SerchByName(string name)const
 	{
 		
-		for (auto iter = cars.begin(); iter != cars.end(); ++iter)
-		{
-			
-		}
+		
 	}
 
 	void SerchByGraduationYear(int graduationYear)const
@@ -95,24 +77,6 @@ public:
 	}
 
 	
-	bool CompareCarByName(const Car& c1, const Car& c2)
-	{
-		return c1.GetName() < c2.GetName();
-	}
-
-	bool CompareCarByGraduationYear(const Car& c1, const Car& c2)
-	{
-		return c1.GetGraduationYear() < c2.GetGraduationYear();
-	}
-
-	bool CompareCarByEngineCapacity(const Car& c1, const Car& c2)
-	{
-		return c1.GetEngineCapacity() < c2.GetEngineCapacity();
-	}
-
-	bool CompareCarByPrice(const Car& c1, const Car& c2)
-	{
-		return c1.GetPrice() < c2.GetPrice();
-	}
+	
 
 };
